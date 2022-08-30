35a61c1e7ca46a1c2216fb915835abc3107f4d3e
Jorge Conejeros <jorgeconejerosrios@gmail.com>
2022-08-30 17:08:06 -0400

    Ejemplos de las funciones en Rcpp

--- example/example.R
+++ example/example.R
@@ -4,17 +4,31 @@ mB <- matrix(12:1, nrow = 3, ncol = 4)
 auxV <- c(13,14,15,16)
 
 # cbind() 
-addColumn_rcpp(mA,auxV) # <- ERROR
+addC(mA,auxV) # <- ERROR
 auxV <- c(13,14,15)
-addColumn_rcpp(mA,auxV) # <- BIEN
+addC(mA,auxV) # <- BIEN
 
 # rbind()
 auxV <- c(13,14,15,16)
-addRow_rcpp(mA,auxV)
+addR(mA,auxV)
 
 # Wich.max()
 SearchMax(mA)
+SearchMax(mB)
 
 # Wich.min()
-SearchMin_Rcpp(mA)
+SearchMin(mA)
+SearchMin(mB)
+
+# MaxMin()
+mA <- matrix(1:16, nrow = 4, ncol = 4)
+mB <- matrix(16:1, nrow = 4, ncol = 4)
+MaxMin(mA,mB)
+
+# Multiplicacion de matrices
+MxM(mA,mB) # <- Error
+mA <- matrix(1:12, nrow = 3, ncol = 4)
+mB <- matrix(12:1, nrow = 4, ncol = 3)
+MxM(mA,mB) # <- Bien
+
 
